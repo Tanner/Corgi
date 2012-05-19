d39773d733c2cb17b6399d6d883cb26983b0e551
Tanner Smith <tanner@tssoftware.net>
2012-05-19 19:48:42 +0200

    Create bus and mutex for bus. Create PC thread and rewrite tristate buffer as a method not in a separate file. Create destroy method before free.

--- sim.c
+++ sim.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "memory.h"
 
 #define ADDRESS_SPACE 0xFFFF
 
 #define REGISTERS_COUNT 8
+
+void sim_tristate_buffer(u32 input, bool enable);
+
+void * sim_pc();
+
+static bool sim_running;
 
 static MEMORY *memory;
 static MEMORY *registers;
@@ -16,7 +23,12 @@ static MEMORY *mar;
 static MEMORY *a;
 static MEMORY *b;
 
-u32 bus;
+static u32 bus;
+static pthread_mutex_t bus_mutex;
+
+static bool dr_pc;
+
+static pthread_t pc_thread;
 
 /**
  * Initialize the simulator.
@@ -30,6 +42,44 @@ void sim_init() {
 
 	a = memory_init(1);
 	b = memory_init(1);
+
+	pthread_mutex_init(&bus_mutex, NULL);
+
+	sim_running = true;
+
+	pthread_create(&pc_thread, NULL, sim_pc, NULL);
+}
+
+void * sim_pc() {
+	while (sim_running) {
+		pthread_mutex_lock(&bus_mutex);
+
+		u32 pc_value = memory_update(pc, 0, bus);
+
+		pthread_mutex_unlock(&bus_mutex);
+
+		sim_tristate_buffer(pc_value, dr_pc);
+	}
+
+	return NULL;
+}
+
+void sim_tristate_buffer(u32 input, bool enable) {
+	if (!enable) {
+		return;
+	}
+
+	pthread_mutex_lock(&bus_mutex);
+
+	bus = input;
+
+	pthread_mutex_unlock(&bus_mutex);
+}
+
+void sim_destroy() {
+	sim_running = false;
+
+	pthread_mutex_destroy(&bus_mutex);
 }
 
 /**
@@ -56,7 +106,10 @@ void sim_free() {
 int main(int argc, char **argv) {
 	sim_init();
 
+	sim_destroy();
 	sim_free();
+
+	pthread_exit(NULL);
 
 	return EXIT_SUCCESS;
 }
