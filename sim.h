6d43efb9dad27e2efee0a46b74adae6345be37c4
Tanner Smith <tanner@tssoftware.net>
2012-05-29 21:26:13 +0200

    Add macro to extract bits 4-0.

--- sim.h
+++ sim.h
@@ -6,10 +6,11 @@
 
 #define CORGI_SIM_H_
 
+#define BITS_15_to_13(A) ((A) >> 12 & 0x7)
 #define BITS_12_to_9(A) (((A) >> 8) & 0xF)
 #define BITS_8_to_5(A) (((A) >> 4) & 0xF)
+#define BITS_4_to_0(A) ((A) & 0x1F)
 #define BITS_3_to_0(A) ((A) & 0xF)
-#define BITS_15_to_13(A) ((A) >> 12 & 0x7)
 
 void sim_tristate_buffer(u32 input, bool enable);
 
