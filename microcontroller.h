aae802362ca262c8ad1879cff9bc52334d5aeccd
Tanner Smith <tanner@tssoftware.net>
2012-06-19 13:28:36 +0200

    Create #defines to retrievie contents of main rom (next state and signals).

--- microcontroller.h
+++ microcontroller.h
@@ -7,6 +7,40 @@
 
 #define CORGI_MICROCONTROLLER_H_
 
+/**
+ * Content of the microcontroller main ROM contains the next state in bits 0-5.
+ */
+#define MICROCONTROLLER_NEXT_STATE(CONTENTS) ((CONTENTS) & 0x1F)
+
+/**
+ * The following content is located at the bits specified in the main rom.
+ */
+#define MICROCONTROLLER_DR_REG 6
+#define MICROCONTROLLER_DR_MEM 7
+#define MICROCONTROLLER_DR_ALU 8
+#define MICROCONTROLLER_DR_PC 9
+#define MICROCONTROLLER_DR_OFF 10
+#define MICROCONTROLLER_LD_PC 11
+#define MICROCONTROLLER_LD_IR 12
+#define MICROCONTROLLER_LD_MAR 13
+#define MICROCONTROLLER_LD_A 14
+#define MICROCONTROLLER_LD_B 15
+#define MICROCONTROLLER_LD_Z 16
+#define MICROCONTROLLER_WR_REG 17
+#define MICROCONTROLLER_WR_MEM 18
+#define MICROCONTROLLER_REG_SEL_LO 19
+#define MICROCONTROLLER_REG_SEL_HI 20
+#define MICROCONTROLLER_ALU_LO 21
+#define MICROCONTROLLER_ALU_HI 22
+#define MICROCONTROLLER_OP_TEST 23
+#define MICROCONTROLLER_CHK_Z 24
+
+/**
+ * Retrievies the bit in the first position in a bitstring after shifting an
+ * amount. 
+ */
+#define MICROCONTROLLER_RETRIEVE_BIT(CONTENTS, SHIFT) ((CONTENTS) >> SHIFT & 0x1)
+
 typedef struct _MICROCONTROLLER {
 	MEMORY *main;
 	MEMORY *sequencer;
